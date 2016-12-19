d0f6a02d9913aaccb5c5de8a01a3ddfa2ed5f5f2
ifo0o <ivostoepker@gmail.com>
2016-12-19 20:05:31 +0100

    Tex button now indicates sending

--- public/javascripts/Tex/mainTex.js
+++ public/javascripts/Tex/mainTex.js
@@ -7,6 +7,8 @@ var main = function() {
 
     $(document).on("click", "#sendtex", function(e){
         var d = {"tex": $('#tex').val()}
+
+        $('#sendtex').text("Sending...")
         console.log(d)
         $.ajax({
           type: 'PUT',
@@ -16,7 +18,12 @@ var main = function() {
         }).done(function( response ){
           //Check if succesful (then reponse will be empty string)
           if (response.msg === ''){
-
+              $('#sendtex').text('');
+                      $('#sendtex').append('<span class="glyphicon glyphicon-send"></span>')
+              var now = new Date();
+              $('.update').remove()
+              $('#tex-container-well').append('<p class="update"> Laatste update om ' + now.toLocaleTimeString() + '</p>')
+            //$('#tex-container-well').append('<div class="alert alert-success">Laatste update om ' + now.getHours() + ':'+ now.getMinutes() + ':'+ now.getSeconds() + '</div>')
           }else{
             alert('Error: ' + response.msg); //If there is an error then alert with error message
           }
